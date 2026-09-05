ada3550df18731ba98f9ff713e93b033c8cc04bf
tompassarelli <tom.passarelli@protonmail.com>
2026-09-05 11:03:36 -0700

    Adopt checked typed readiness laws for command feedback

--- scripts/clause-pin.ts
+++ scripts/clause-pin.ts
@@ -1,7 +1,7 @@
 export const CLAUSE_COMMIT =
-  "e626484d25894d33290c4327ed0470e7525a98e6";
+  "4634bec6f0d854ab95f911a615d059bd035e3e2c";
 
 export const CLAUSE_WASM_SHA256 =
-  "7d9dcadded16384b3f20467566f2a4893425f134b4eb9bca3623f2fe5bfb0425";
+  "94324b488bab5b8027fee151a4f4972404f89b251bfc7f0cda2a380248794145";
 
 export const CLAUSE_ROOT = new URL("../vendor/clause/", import.meta.url).pathname;
