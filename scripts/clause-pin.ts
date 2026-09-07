7bc24eb666a6ce626d772c2321de6457ef7b7fd0
tompassarelli <tom.passarelli@protonmail.com>
2026-09-07 14:23:56 -0700

    Consume bulk snapshot decoding and bounded effect-plan lookup

--- scripts/clause-pin.ts
+++ scripts/clause-pin.ts
@@ -1,7 +1,7 @@
 export const CLAUSE_COMMIT =
-  "adf379db6cc6a7898e1ce7f21b96363bfe0c291c";
+  "5e2decb9a6ea9fab2538181968d3532a956877f2";
 
 export const CLAUSE_WASM_SHA256 =
-  "af962bb292ea19644f654b3ac741401974de67840dc3aaa11f8f385a76f0e604";
+  "d5254faacb06e9d16aafc9abdc09fda64590b599509431fd371a27492bd744eb";
 
 export const CLAUSE_ROOT = new URL("../vendor/clause/", import.meta.url).pathname;
