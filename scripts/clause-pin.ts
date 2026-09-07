015f971b5f01051914e36b1e22fd74e4a62df2f1
tompassarelli <tom.passarelli@protonmail.com>
2026-09-07 16:13:42 -0700

    Pin measured contiguous match binding storage

--- scripts/clause-pin.ts
+++ scripts/clause-pin.ts
@@ -1,7 +1,7 @@
 export const CLAUSE_COMMIT =
-  "b2766424a52fed37ac34a9ab0a27a0380d7a47ec";
+  "f6c1fbea4065cd256ebbdf988a310c85806e0a1b";
 
 export const CLAUSE_WASM_SHA256 =
-  "cfa60869c574de24005890dee23e4bfd2e06b896bb3f0a98169bd3bd7d69630f";
+  "80830dc93a380be4bc4a3d5f50cfc170541f41e105812d66a09d7a9957024a50";
 
 export const CLAUSE_ROOT = new URL("../vendor/clause/", import.meta.url).pathname;
