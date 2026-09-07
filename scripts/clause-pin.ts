8d63925faafe78598d36717ccba1402a1a67f260
tompassarelli <tom.passarelli@protonmail.com>
2026-09-07 16:50:05 -0700

    Pin measured effect-coordinate plan retention

--- scripts/clause-pin.ts
+++ scripts/clause-pin.ts
@@ -1,7 +1,7 @@
 export const CLAUSE_COMMIT =
-  "a9bfe50739ec63549027020fcb801ebca3154284";
+  "078a0af4a449e2211645037d2c2569e402ddaa0a";
 
 export const CLAUSE_WASM_SHA256 =
-  "80830dc93a380be4bc4a3d5f50cfc170541f41e105812d66a09d7a9957024a50";
+  "07118d3c137c891c79f58dc13cda7a3e2011c82ae9da82a39c317f65d6a80aff";
 
 export const CLAUSE_ROOT = new URL("../vendor/clause/", import.meta.url).pathname;
