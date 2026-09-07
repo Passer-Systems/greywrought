87e65addea223f2029fd076c4806c8c945b29648
tompassarelli <tom.passarelli@protonmail.com>
2026-09-07 16:05:14 -0700

    Pin measured reuse of unchanged projected terms

--- scripts/clause-pin.ts
+++ scripts/clause-pin.ts
@@ -1,7 +1,7 @@
 export const CLAUSE_COMMIT =
-  "d105c83a9496765463aa8f2760292f4aaa4f16aa";
+  "b2766424a52fed37ac34a9ab0a27a0380d7a47ec";
 
 export const CLAUSE_WASM_SHA256 =
-  "68677b8cf4af6b00cac5b605eb0dfaea73170d99220bf1168f595245805c79a5";
+  "cfa60869c574de24005890dee23e4bfd2e06b896bb3f0a98169bd3bd7d69630f";
 
 export const CLAUSE_ROOT = new URL("../vendor/clause/", import.meta.url).pathname;
