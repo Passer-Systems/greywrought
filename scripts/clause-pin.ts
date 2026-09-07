fa6ad02961ae01bd7986998b7aac0441ed73bbdd
tompassarelli <tom.passarelli@protonmail.com>
2026-09-07 15:20:16 -0700

    Pin typed source replacement and immutable projection decoder

--- scripts/clause-pin.ts
+++ scripts/clause-pin.ts
@@ -1,7 +1,7 @@
 export const CLAUSE_COMMIT =
-  "46ad2bb01f4313c632cd192ded1bb95accb5f3ff";
+  "9360cf486fb7975467637cbaa875d781de1aec19";
 
 export const CLAUSE_WASM_SHA256 =
-  "e5a834ac7cfa7de6b59f6cfabf9822c8003b36a6aabc2a6fbeb7deadeb2412a0";
+  "4c4c206973c5105aee662b4f848e29f76895f82da16bfddd6f663e6cec6c1bd7";
 
 export const CLAUSE_ROOT = new URL("../vendor/clause/", import.meta.url).pathname;
