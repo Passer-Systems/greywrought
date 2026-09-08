a1af795b719e84f68261c40f385eb256dacb3ab9
tompassarelli <tom.passarelli@protonmail.com>
2026-09-07 23:23:58 -0700

    Pin checked source facet-rebinding reuse

--- scripts/clause-pin.ts
+++ scripts/clause-pin.ts
@@ -1,7 +1,7 @@
 export const CLAUSE_COMMIT =
-  "df31222a9ebab5064ef37c20e8724caec372d05a";
+  "bed2e181ba7932fc9fb0e81ab7373ebeacf651a6";
 
 export const CLAUSE_WASM_SHA256 =
-  "5923b242cac3bfe0a5b1eb81bc1362c09591421bfa86703e2512116746c849c6";
+  "e46600cc79fd7af7faadac06d45cd319ab546149809afd191f0c484f1b7cec4d";
 
 export const CLAUSE_ROOT = new URL("../vendor/clause/", import.meta.url).pathname;
