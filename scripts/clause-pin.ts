82ac1cbb6cc91536b6ce80ca36d5298c19d87ccd
tompassarelli <tom.passarelli@protonmail.com>
2026-09-08 03:55:51 -0700

    Pin assembled text primitives and checked package reuse

--- scripts/clause-pin.ts
+++ scripts/clause-pin.ts
@@ -1,7 +1,7 @@
 export const CLAUSE_COMMIT =
-  "aec9627f8129cddc9d2ad1bf0b48ae0ce003be0d";
+  "d67de9a9407fc1d6e9cce1b7c10ea153d6630771";
 
 export const CLAUSE_WASM_SHA256 =
-  "85cceef924457c5eef4a685446891a2aebf296bcf55395ab8990030ca30e7023";
+  "fae8dc4644a3c38f137961dae4e9c569576e6ffb5f77410749257e80aa71adfe";
 
 export const CLAUSE_ROOT = new URL("../vendor/clause/", import.meta.url).pathname;
