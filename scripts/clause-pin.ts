4ace3d670b105ca30065c6e4474feea779b08fd8
tompassarelli <tom.passarelli@protonmail.com>
2026-09-08 14:55:05 -0700

    Restore saved journeys with signed zero values

--- scripts/clause-pin.ts
+++ scripts/clause-pin.ts
@@ -1,4 +1,4 @@
 export const CLAUSE_COMMIT =
-  "9894bb5b1e7b0f9a4035f7903a5117db121cdef4";
+  "349f44c73c8d4d56f8cfd57c5ca6f7af1b6f9569";
 
 export const CLAUSE_ROOT = new URL("../vendor/clause/", import.meta.url).pathname;
