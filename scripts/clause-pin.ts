be462d24a24cefd94c07bc36f4e8e2cd61591979
tompassarelli <tom.passarelli@protonmail.com>
2026-09-07 23:30:35 -0700

    Consume typed dictionaries and alternative composition runtime

--- scripts/clause-pin.ts
+++ scripts/clause-pin.ts
@@ -1,7 +1,7 @@
 export const CLAUSE_COMMIT =
-  "df31222a9ebab5064ef37c20e8724caec372d05a";
+  "761db738579c9a042bfe8f238fd20472f0e7a178";
 
 export const CLAUSE_WASM_SHA256 =
-  "5923b242cac3bfe0a5b1eb81bc1362c09591421bfa86703e2512116746c849c6";
+  "8ff962ad22b848b25ee6e0b138a5a359070aa952b60add7715f6ecef4686d80b";
 
 export const CLAUSE_ROOT = new URL("../vendor/clause/", import.meta.url).pathname;
