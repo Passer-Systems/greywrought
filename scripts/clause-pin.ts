cd8b2c37fa5005234b1bb021fcc153aa5f104864
tompassarelli <tom.passarelli@protonmail.com>
2026-09-08 00:23:48 -0700

    Pin compiler-owned package checking runtime

--- scripts/clause-pin.ts
+++ scripts/clause-pin.ts
@@ -1,7 +1,7 @@
 export const CLAUSE_COMMIT =
-  "444037d65f3dd08aed3970acb309fdad84d5639a";
+  "aec9627f8129cddc9d2ad1bf0b48ae0ce003be0d";
 
 export const CLAUSE_WASM_SHA256 =
-  "1a90f3474f22c4d56516eedf8f47e510d4a53a47f1632b56da8360c238de55a2";
+  "85cceef924457c5eef4a685446891a2aebf296bcf55395ab8990030ca30e7023";
 
 export const CLAUSE_ROOT = new URL("../vendor/clause/", import.meta.url).pathname;
