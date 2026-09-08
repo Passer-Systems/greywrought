d406d916cbadd92b5ffdbfab19b6b0a1fa8d456a
tompassarelli <tom.passarelli@protonmail.com>
2026-09-07 21:06:09 -0700

    Pin assembled source compiler and runtime for combined game delivery

--- scripts/clause-pin.ts
+++ scripts/clause-pin.ts
@@ -1,7 +1,7 @@
 export const CLAUSE_COMMIT =
-  "bfe56622f3863ea142471bf0c0e430ee51aca4bc";
+  "14ebb0dda407086e0b1d9a48b928ec8a40f5564e";
 
 export const CLAUSE_WASM_SHA256 =
-  "e6d45a5da724a6bab6b44b3c39e8afb2ad4d08be861ac476f00e05450b1a6d69";
+  "57d4e30d59263cfd44dc584937a71567563f28687e704efb1a3203af877a3d36";
 
 export const CLAUSE_ROOT = new URL("../vendor/clause/", import.meta.url).pathname;
