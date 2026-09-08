9dd90619179641de1b3593b82e5cfff4d5360057
tompassarelli <tom.passarelli@protonmail.com>
2026-09-07 18:37:59 -0700

    Pin complete compact source continuity and retained source analysis

--- scripts/clause-pin.ts
+++ scripts/clause-pin.ts
@@ -1,7 +1,7 @@
 export const CLAUSE_COMMIT =
-  "f279b34d91fe083f1d9f1419b4c594bdf3d20deb";
+  "380be52cb17d2d6e61ff7ce93391d3b74b841ee1";
 
 export const CLAUSE_WASM_SHA256 =
-  "3a26f08493fe65e914be26695f278252dbe1f0701acb324346f7d27ceb3e9839";
+  "22583bac74e6ac1460dd27a716715f379a39586d6dd1c12b5f1d564cb47a0ffd";
 
 export const CLAUSE_ROOT = new URL("../vendor/clause/", import.meta.url).pathname;
