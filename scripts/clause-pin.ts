b99528f28c03c9d9617aa58b914b7af3eed7882e
tompassarelli <tom.passarelli@protonmail.com>
2026-09-07 22:40:57 -0700

    Consume retained canonical size checks in game admission

--- scripts/clause-pin.ts
+++ scripts/clause-pin.ts
@@ -1,7 +1,7 @@
 export const CLAUSE_COMMIT =
-  "9a75cbb9ee3fd82a5bbc80c1b8c908a223328737";
+  "8b8a95f42031d276cdc14e7348e7f96c2447f3ab";
 
 export const CLAUSE_WASM_SHA256 =
-  "e586bf66b0ca4cb10e65063d7ede4d05d78e218b180738a7f74f6d9cd045b3e5";
+  "2128d917c8e2480b21b0d9a6d07a3cc38fdbf7f2fe150de11fa9bb0f960aa9c0";
 
 export const CLAUSE_ROOT = new URL("../vendor/clause/", import.meta.url).pathname;
