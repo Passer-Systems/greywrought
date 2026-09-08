5618eb1abb2f55ed6a6db4ec0038f58010534a0c
tompassarelli <tom.passarelli@protonmail.com>
2026-09-08 06:06:39 -0700

    Use complete bulk diagnostics for game explanations

--- scripts/clause-pin.ts
+++ scripts/clause-pin.ts
@@ -1,7 +1,7 @@
 export const CLAUSE_COMMIT =
-  "10e618aa177c30ef7071b5a90ee7bf082eec6a26";
+  "fa86f5c2ad585d3c3262f7b7cbf36781e1c7a2bc";
 
 export const CLAUSE_WASM_SHA256 =
-  "1883b36e68bd9f2998d79abb6637023b8f5fe91f400f1b1f5e38253c741a918b";
+  "e245ddaeae767ddd92e9e283bc075c1b91b07d9d493f07f34af1ea844adb50d5";
 
 export const CLAUSE_ROOT = new URL("../vendor/clause/", import.meta.url).pathname;
