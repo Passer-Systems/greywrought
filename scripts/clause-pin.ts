a2a52b92dbc22aaa2c8a90c61c911462aafe7302
tompassarelli <tom.passarelli@protonmail.com>
2026-09-07 22:19:51 -0700

    Pin retained checkpoint restoration and checked quoted fields

--- scripts/clause-pin.ts
+++ scripts/clause-pin.ts
@@ -1,7 +1,7 @@
 export const CLAUSE_COMMIT =
-  "884c61d811864c1f80886db8b54d6891b4b36d67";
+  "9a75cbb9ee3fd82a5bbc80c1b8c908a223328737";
 
 export const CLAUSE_WASM_SHA256 =
-  "b5021012753d6ea42b9dd021e98822f74496f829324f90e53a8cce12bd36b4d8";
+  "e586bf66b0ca4cb10e65063d7ede4d05d78e218b180738a7f74f6d9cd045b3e5";
 
 export const CLAUSE_ROOT = new URL("../vendor/clause/", import.meta.url).pathname;
