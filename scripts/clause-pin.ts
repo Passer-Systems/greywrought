6d7033f615fdac66a440feefb31affe7cad8622c
tompassarelli <tom.passarelli@protonmail.com>
2026-09-08 14:44:39 -0700

    Use overlapping checked-source derivation in native game

--- scripts/clause-pin.ts
+++ scripts/clause-pin.ts
@@ -1,4 +1,4 @@
 export const CLAUSE_COMMIT =
-  "802f9780780f63247789d5cea2d26edd11de0a27";
+  "9894bb5b1e7b0f9a4035f7903a5117db121cdef4";
 
 export const CLAUSE_ROOT = new URL("../vendor/clause/", import.meta.url).pathname;
